c479e6b21dddbf2cc1ac98ede24577559ea72527
Sirknightfarron <marvintrimpop@web.de>
2018-02-02 01:45:53 +0100

    Add files via upload

--- ShuffleNorris.js
+++ ShuffleNorris.js
@@ -6,7 +6,7 @@
   //Variable zum Speichern der Jokes
       var jokeArray = [];
   //die Anzahl der zu ladenden Jokes
-        var jokeCount = 5;
+      var jokeCount = 5;
 
     //GET funktion zum Anfragen der Daten und Speicherung in einem jokeArray
     $(document).ready(
@@ -26,39 +26,33 @@
       }
     }
 
-    var offakk;
-    var offtail;
-    var akk = [];
-    var tail = [];
 
-    //gibt alle gesammelten Jokes in einer nicht zusammenhängenden Wortreihenfolge aus.
-    function giveJokesRandomly(){
+//gibt alle gesammelten Jokes in einer nicht zusammenhängenden Wortreihenfolge aus.
+function giveJokesRandomly(){
+  console.log(randomize(jokesToWords()));
+}
 
-    for (var i = 0; i < jokeArray.length; i++) {
-      kopfAbschneiden(jokeArray[i]);
+    function jokesToWords(){
+      var akk = [];
+      akk = jokeArray.join();
+      return akk.split('\u0020');
     }
-  }
-    offakk = akk;
-    offtail = tail;
-    //Diese Funktion wird lediglich hier benötigt und trennt ab dem ersten "space"
-    //das Wort von dem rest des Arrays und der rest wird in tail gespeichert
-    function kopfAbschneiden(str){
-      for (var i = 0; i < str.length; i++) {
-        if (!tail.includes('\u0020')) {
-        return akk = akk.concat(str);
 
-        } else if (str.charAt(i) == '\u0020'){
-          tail = str.substring(i, str.length);
-          akk = akk.concat(str.substring(0, i));
-        } else {
-          kopfAbschneiden(tail);
-        }
-        }
+
+//tauscht inhalte von Referenzen in dem Array aus, solange es noch Zahlen zum
+//tauschen gint (currentIndex)
+    function randomize(arr) {
+      var currentIndex = arr.length, tempVal, randIndex;
+
+      while (0 !== currentIndex) {
+
+        randIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        tempVal = arr[currentIndex];
+        arr[currentIndex] = arr[randIndex];
+        arr[randIndex] = tempVal;
       }
 
-
-
-
-
-    //"Since 1940, the year Chuck Norris was born, roundhouse kick related
-// deaths have increased 13,000 percent."
+      return arr;
+    }
